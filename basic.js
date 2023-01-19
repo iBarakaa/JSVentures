5e925dc9de4bc16642117355753fb44608c8855a
iBarakaa <isaacbaraka1@gmail.com>
2023-01-19 23:47:47 +0300

    First var declaration in JS

--- basic.js
+++ basic.js
@@ -1,1 +1,5 @@
 // javascript has 8 variables
+/* the variables are as follows:
+number , bigint , undefined, boolean, string, null, symbol, object */
+
+var myName; //this is an instance of variable declaration in JS
