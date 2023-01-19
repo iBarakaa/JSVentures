fbdf14896fcc7cd22519278ed7579695d0d93ccb
iBarakaa <isaacbaraka1@gmail.com>
2023-01-20 00:08:35 +0300

    Slight edits to basic.js

--- basic.js
+++ basic.js
@@ -1,5 +1,9 @@
 // javascript has 8 variables
 /* the variables are as follows:
-number , bigint , undefined, boolean, string, null, symbol, object */
+number,
+bigint,
+undefined - initial value of all variables. Upon math ops there is a result of NaN,
+boolean, string, null, symbol, object */
 
-var myName; //this is an instance of variable declaration in JS
+var a = 9; //variable declaration and assignment
+var camelCase = "camelCaseOne is indeed JS best practise" //this is an instance of camelCase in action as JS is case sensitive
