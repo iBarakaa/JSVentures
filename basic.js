e658002750cff2c9f1e34a0b322b7bc1d724e2bd
iBarakaa <isaacbaraka1@gmail.com>
2023-01-20 20:50:26 +0300

    Printing output in js

--- basic.js
+++ basic.js
@@ -6,4 +6,5 @@ undefined - initial value of all variables. Upon math ops there is a result of N
 boolean, string, null, symbol, object */
 
 var a = 9; //variable declaration and assignment
-var camelCase = "camelCaseOne is indeed JS best practise" //this is an instance of camelCase in action as JS is case sensitive
+var camelCase = "camelCaseOne is indeed JS best practise"; //this is an instance of camelCase in action as JS is case sensitive
+console.log(a);
