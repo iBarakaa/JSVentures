9ab13091bd214c78c413be82c9bf5ef2748895ac
iBarakaa <isaacbaraka1@gmail.com>
2023-01-24 22:32:37 +0300

    Simple shopping list using arrays

---
+++ shoppinglist.js
@@ -0,0 +1,9 @@
+//simple shopping list in JS using basic multidimensional arrays
+const myList = [
+    ["Dairy Fresh", 20],
+    ["Skyn", 10],
+    ["Konyagi", 2],
+    ["1/2 Chicken", 4],
+    ["Newspaper", 1]
+  ];
+  console.log(myList);
