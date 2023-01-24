65e50abed3705aad4ce7b8526220253c7d5a2e82
iBarakaa <isaacbaraka1@gmail.com>
2023-01-24 21:39:13 +0300

    array indexing

--- arrays.js
+++ arrays.js
@@ -5,3 +5,8 @@ console.log(array);
 //multidimensional array
 const mArray = [['Manchester City', 'Arsenal'],['Real Madrid','Barcelona']];
 console.log(mArray);
+
+//for array indexes instead of fetching character location, it fetches the entry in entirity
+const iarray = [50, 60, 70];
+const myData = iarray[0];
+console.log(myData);
