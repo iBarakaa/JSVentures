2f19ab77bcd4aa62e6d50677681aefcfd4793b39
iBarakaa <isaacbaraka1@gmail.com>
2023-01-24 21:13:03 +0300

    Intro to arrays

---
+++ arrays.js
@@ -0,0 +1,7 @@
+//simple instance of an array
+const array = ['I am number', '4'];
+console.log(array);
+
+//multidimensional array
+const mArray = [['Manchester City', 'Arsenal'],['Real Madrid','Barcelona']];
+console.log(mArray);
