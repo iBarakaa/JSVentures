74d169ed57ac28e73aec12d3319c3bebe4552e2b
iBarakaa <isaacbaraka1@gmail.com>
2023-01-26 22:33:16 +0300

    Return statement in js function

--- functions.js
+++ functions.js
@@ -7,8 +7,15 @@ reusableFunction();
 
 //introducing arguments into functions
 function functionWithArgs(a , b) {
-    sum = a + b;
+    let sum = a + b;
     console.log(sum);
 }
 
 functionWithArgs(10, 20);
+
+//functions with return values
+function timesFive(num) {
+    return num * 5;
+}
+
+timesFive(5);
