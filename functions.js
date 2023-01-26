3e9990f2d6ad58497801424d2e5fc5c794b77bbf
iBarakaa <isaacbaraka1@gmail.com>
2023-01-26 22:46:19 +0300

    Testing global scope of variables

--- functions.js
+++ functions.js
@@ -19,3 +19,26 @@ function timesFive(num) {
 }
 
 console.log(timesFive(5));
+
+/* variables defined outside a function have a global scope (seen wherever in JS code)
+vars declared without let or const are created in global scope by default
+Therefore, one should always use let or const in variable naming */
+//instance of global scope use cases
+let myGlobal = 10;
+
+function fun1() {
+  oopsGlobal = 5; // this assumes the characteristics of a global variable
+}
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+fun1();
+fun2();
