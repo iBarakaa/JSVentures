036c2ffe3481276b00436b586e5197dbafdd4a4c
iBarakaa <isaacbaraka1@gmail.com>
2023-01-26 22:26:46 +0300

    Function with arguments

--- functions.js
+++ functions.js
@@ -2,3 +2,13 @@
 function reusableFunction() {
     console.log('Hi World');
 }
+
+reusableFunction();
+
+//introducing arguments into functions
+function functionWithArgs(a , b) {
+    sum = a + b;
+    console.log(sum);
+}
+
+functionWithArgs(10, 20);
