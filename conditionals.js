8323722bd33b57f12b0691857cc0aedd32876d91
iBarakaa <isaacbaraka1@gmail.com>
2023-01-28 22:39:39 +0300

    Understanding basic if statement usage in JS

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+//instance of conditional if statements in Js
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue == true) {
+      return "Yes, that was true"
+    }
+    return 'No, that was false'
+  }
