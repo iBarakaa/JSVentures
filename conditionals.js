86645cdae80d192787751779eec2addb650eaea3
iBarakaa <isaacbaraka1@gmail.com>
2023-01-28 22:43:38 +0300

    Updated if statement usagee

--- conditionals.js
+++ conditionals.js
@@ -2,7 +2,10 @@
 function trueOrFalse(wasThatTrue) {
     // Only change code below this line
     if (wasThatTrue == true) {
-      return "Yes, that was true"
+      return "Yes, that was true";
     }
-    return 'No, that was false'
+    return 'No, that was false';
   }
+
+//trueOrFalse(true); uncomment this line to run output for the code above
+
