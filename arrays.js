45da7aa6fcde7776bbe53a1cf7d9157cd27b0e13
iBarakaa <isaacbaraka1@gmail.com>
2023-01-28 22:24:16 +0300

    Function of queue in arrays

--- arrays.js
+++ arrays.js
@@ -47,3 +47,16 @@ console.log(pArray);
 const addFirstEntry = pArray.unshift(['Paul', 35]);
 console.log(pArray);
 
+//function of queue in array
+function nextInLine(arr, item) {
+  arr.push(item); 
+  let removedItem = arr.shift();
+  return removedItem; 
+}
+
+// test instance
+let testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
