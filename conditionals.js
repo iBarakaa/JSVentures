f285d6c460f0b13454f09d33f37c92b5af2fd9f6
iBarakaa <isaacbaraka1@gmail.com>
2023-01-30 09:22:12 +0300

    Equality and inequality operators

--- conditionals.js
+++ conditionals.js
@@ -31,4 +31,25 @@ console.log(testStrict(10));
 console.log(typeof 3);
 console.log(typeof '3');
 
+//inequality operator tests
+function testNotEqual(val) {
+    if (val != 99) { //this can also be substituted with !== for strict operations
+        return 'Not Equal';
+    }
+    return 'Equal';
+}
+console.log(testNotEqual(10));
+
+//> < operators
+function greaterThan(val) {
+    if (val > 100) {
+        return 'Greater than 100';
+    }
+    if (val > 10) { //in the event we use >=, this includes the value of 10 itself
+        return 'Greater than 10';
+    }
+    return '10 or less';
+}
+console.log(greaterThan(11));
+//notice than if statements check code systematically hence order matters
 
