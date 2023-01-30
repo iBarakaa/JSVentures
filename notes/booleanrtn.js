8b1eed8ffbb325fe1231dea3affc11df7a1976e3
iBarakaa <isaacbaraka1@gmail.com>
2023-01-30 12:13:11 +0300

    Simple method of returning boolean results

---
+++ notes/booleanrtn.js
@@ -0,0 +1,6 @@
+// work flow of how to return boolean functions 
+function findingOutIfLess(a,b) {
+    return a < b; // saves bulk of coding if statement to return t/f values
+}
+
+console.log(findingOutIfLess(10,5)); 
