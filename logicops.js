a25d7f8ae2477aa83f6e4b428eb7d2cae93162dd
iBarakaa <isaacbaraka1@gmail.com>
2023-01-30 09:47:50 +0300

    Or logical operations instance

--- logicops.js
+++ logicops.js
@@ -7,3 +7,13 @@ function testLogicalAND(val) {
     return 'No';
 }
 console.log(testLogicalAND(27));
+
+// if either is true, we use || operator (OR operator)
+//this function only returns yes to values btwn and including 10 and 20
+function testLogicalOR(val) {
+    if(val < 10 || val > 20) {
+        return 'Outside';
+    }
+    return 'Inside';
+}
+console.log(testLogicalOR(15));
