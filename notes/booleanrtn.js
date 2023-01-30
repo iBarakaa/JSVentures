4599b8428576b5acce456976b32bcabe43065ed7
iBarakaa <isaacbaraka1@gmail.com>
2023-01-30 12:21:06 +0300

    Updated with instance of returning undefined values

--- notes/booleanrtn.js
+++ notes/booleanrtn.js
@@ -4,3 +4,17 @@ function findingOutIfLess(a,b) {
 }
 
 console.log(findingOutIfLess(10,5)); 
+
+/* we can return undefined values in two ways
+1. return undefined;
+2. return; */
+// Instance of code where if a or b is less than 0, the function returns undefined
+function abTest(a, b) {
+    if (a < 0 || b < 0) {
+      return; //or return undefined;
+    }
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  console.log(abTest(-2,-2));
