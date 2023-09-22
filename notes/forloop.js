08ab0088d7ab655d6b225b369e5694c49a801002
iBarakaa <isaacbaraka1@gmail.com>
2023-09-22 12:57:39 +0300

    nested for loops

--- notes/forloop.js
+++ notes/forloop.js
@@ -30,3 +30,30 @@ for (let i=9; i>=1; i-=2) {
 }
 
 console.log(roddArray);
+
+// adding elements of an array to variable
+const totalArray = [2,3,4,5,6];
+let total = 0;
+
+// i stands as zero because arrays are zero indexed (start at 0)
+for (let i = 0; i < totalArray.length; i++) {
+    total = total + totalArray[i];
+}
+
+console.log(total);
+
+// nesting for loops
+// multiplicationn of all numbers in sub-arrays
+
+function multiplyAll(arr) {
+    let product = 1;
+
+    for (let i=0; i<arr.length; i++){
+        for (let j=0; j<arr[i].length; j++) {
+            product = product * arr[i][j];
+        }
+    }
+    return product;
+}
+
+console.log(multiplyAll([[1,2], [3,4], [5,6,7],]));
