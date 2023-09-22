d96f86933f9c182a4888f17db65e09989c01a446
iBarakaa <isaacbaraka1@gmail.com>
2023-09-22 14:36:24 +0300

    Understanding the concept of recursion in JS

---
+++ notes2/recursion.js
@@ -0,0 +1,17 @@
+// recursion - function can be expressed in terms of itself
+// multiply(arr, n) [product of array elements up to element n] == mutiply(arr, n-1)[multiplies elements of array from index 0 to index 'n-2'] * arr[n-1] [last element of the intended scope]
+
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0;
+    }
+
+    // the base case prevents infinite execution 
+    else {
+        return sum(arr, n-1) + arr[n-1];
+    }
+}
+
+let nums = [2,3,4,5];
+
+console.log(sum(nums, 3));
