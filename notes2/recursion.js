e8263200a70acf47267a7611abf7732d209396d4
iBarakaa <isaacbaraka1@gmail.com>
2023-09-24 23:30:09 +0300

    Key comments on recursion

--- notes2/recursion.js
+++ notes2/recursion.js
@@ -1,6 +1,8 @@
 // recursion - function can be expressed in terms of itself
-// multiply(arr, n) [product of array elements up to element n] == mutiply(arr, n-1)[multiplies elements of array from index 0 to index 'n-2'] * arr[n-1] [last element of the intended scope]
-
+// multiply(arr, n) [product of array elements up to element n] 
+// == mutiply(arr, n-1)[multiplies elements of array from index 0 to index 'n-2'] * arr[n-1] [last element of the intended scope]
+// the trick to understanding recursion is to understand that once we enter the recursive call, 
+// we exit it once we arrive the base case and the code execution is continued
 function sum(arr, n) {
     if (n <= 0) {
         return 0;
