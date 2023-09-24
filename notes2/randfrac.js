8292d016178646623da4282366c0f0e462e6eae7
iBarakaa <isaacbaraka1@gmail.com>
2023-09-24 20:21:03 +0300

    Comments to random fractions generation

--- notes2/randfrac.js
+++ notes2/randfrac.js
@@ -1,8 +1,8 @@
 // generation of random fractions 
-// done with the function math.random()
+// done with the function math.random() that returns a random decimal 0 inclusive to 1(exclusive)
 
 function randomFraction() {
-    return Math.random();
+    return Math.random(); 
 }
 
 console.log(randomFraction());
